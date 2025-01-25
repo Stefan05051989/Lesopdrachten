4895f1dafeb2de2d37870ebf66ea4c88bf391ca7
Stefan <stefan.kiers89@hotmail.com>
2025-01-25 15:23:02 +0100

    arrays

---
+++ src/tabelloops/foreachvsforloops.java
@@ -0,0 +1,20 @@
+package tabelloops;
+
+public class foreachvsforloops {
+    public static void main(String[] args) {
+        int[] lijst = {1,2,3,4};
+        int som1 = 0, som2 = 0;
+
+        for(int i = 0; i < lijst.length; i++) {
+            som1 = som1 + lijst[i];
+        }
+
+        for(int getal : lijst) {
+            som2 = som2 + getal;
+        }
+
+        System.out.println("som1: " + som1); // som1: 10
+        System.out.println("som2: " + som2); // som2: 10
+
+    }
+}
