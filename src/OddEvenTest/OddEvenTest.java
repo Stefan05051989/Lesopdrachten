1262860fbd7ee122d5426956f10b2552c0e43eb8
Stefan <stefan.kiers89@hotmail.com>
2025-01-25 13:14:48 +0100

    even oneven test met scanner

---
+++ src/OddEvenTest/OddEvenTest.java
@@ -0,0 +1,18 @@
+package OddEvenTest;
+
+import java.util.Scanner;
+
+public class OddEvenTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welkom, geef nummer om conditie te controleren : ");
+        int input = scanner.nextInt();
+
+
+        if (input % 2 == 0){
+            System.out.println(input + " het is een even nummer.");
+        }else{
+            System.out.println(input + " het nummer is oneven.");
+        }
+    }
+}
